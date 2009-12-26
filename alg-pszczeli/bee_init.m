f05ee2b015f3601ca475dd9bf8e2875723199847
feanaro666 <feanaro666@3277dffe-d080-11de-97ae-379497272b75>
2009-12-26 15:13:03 +0000

    Ostateczna z pszczelim

--- alg-pszczeli/bee_init.m
+++ alg-pszczeli/bee_init.m
@@ -1,40 +1,31 @@
-function bee_init(matrix,c)
-clc;
-global cost_matrix n gen_size workers scouts paths_count best_solution_val show_avr show_path;
-global time_delay potential_paths paths_values step;
+function bee_init(matrix,c,workers,scouts,paths_count)
+%workers - liczba robotnic w populacji
+%scouts - liczba zwiadowcow
+%paths_count - liczba wybieranych dróg - musi byæ mniejsza od zwiadowców
 
-n=c;
-cost_matrix = matrix;
-step=0;
-gen_size = length(cost_matrix)-n+1;
+global BEE_cost_matrix BEE_n BEE_gen_size BEE_workers BEE_scouts BEE_paths_count BEE_best_solution_val;
+global BEE_potential_paths BEE_paths_values BEE_step;
+BEE_scouts = scouts;
+BEE_paths_count = paths_count;
+BEE_workers = workers;
+BEE_n=c;
+BEE_cost_matrix = matrix;
+BEE_step=0;
+BEE_gen_size = length(BEE_cost_matrix)-BEE_n+1;
 
-%[cost_matrix x y] = real_gen(gen_size,n);   %generator macierzy kosztow
 
-workers = 400;      %liczba robotnic w populacji
-scouts = 150;        %liczba zwiadowcow
-paths_count = 50;    %liczba wybieranych dróg - musi byæ mniejsza od zwiadowców
-best_solution_val = inf;
 
-%save('cost_matrix_test.mat', 'cost_matrix','gen_size', 'x', 'y', 'n');
-%load('cost_matrix_test.mat');          %wczytanie macierzy kosztow
-
-%parametry wyswietlania
-show_avr = 1;       %pokazuje œredni¹ wartoœæ najlepszego zwiadowcy
-show_path = 1;      %pokazuje najlepsz¹ trasê na grafie 3D
-time_delay = 0.01;  %opóŸnienie miêdzy iteracjami
+BEE_best_solution_val = inf;
 
 %1. Inicjalizacja populacji za pomoc¹ losowych rozwi¹zañ.
-for i=1:scouts
-    potential_paths = [potential_paths random_solution(gen_size,n)'];
+for i=1:BEE_scouts
+    BEE_potential_paths = [BEE_potential_paths random_solution(BEE_gen_size,BEE_n)'];
 end
 best_scout=inf;
 
 %2. Wyliczenie funkcji celu dla populacji.
-for i=1:scouts
-    tmp = goal(potential_paths(:,end-i+1),n,gen_size,cost_matrix);
+for i=1:BEE_scouts
+    tmp = goal(BEE_potential_paths(:,end-i+1),BEE_n,BEE_gen_size,BEE_cost_matrix);
     if(best_scout>tmp) best_scout=tmp;end;
-    paths_values = [paths_values tmp];
+    BEE_paths_values = [BEE_paths_values tmp];
 end
-
-%3. While (kryterium stopu nie spe³nione) //Tworzenie nowej populacji.
-%prealokacja
--- alg-pszczeli/bee_step.m
+++ alg-pszczeli/bee_step.m
@@ -2,92 +2,92 @@ function [out_best_solution out_best_solution_val] = bee_step
 %BEE_STEP Summary of this function goes here
 %   Detailed explanation goes here
 
-global cost_matrix avr n gen_size workers scouts paths_count x y best_solution best_solution_val show_avr show_path;
-global time_delay potential_paths paths_values step;
+global BEE_cost_matrix BEE_n BEE_gen_size BEE_workers BEE_scouts BEE_paths_count BEE_best_solution BEE_best_solution_val;
+global BEE_potential_paths BEE_paths_values BEE_step;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% PETLA GLOWNA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-step=1+step;
+BEE_step=1+BEE_step;
     
     %4.   Wybór najlepszych miejsc do przeszukiwania s¹siedztwa.
-    best_paths_values = zeros(1, paths_count);
-    best_paths = zeros(gen_size+n-1, paths_count);
-    for i=1:paths_count
-        [v index] = min(paths_values);
-        best_paths(:,i) = potential_paths(:,index);
-        best_paths_values(i) = v;
-        paths_values(index) = inf;
+    best_BEE_paths_values = zeros(1, BEE_paths_count);
+    best_paths = zeros(BEE_gen_size+BEE_n-1, BEE_paths_count);
+    for i=1:BEE_paths_count
+        [v index] = min(BEE_paths_values);
+        best_paths(:,i) = BEE_potential_paths(:,index);
+        best_BEE_paths_values(i) = v;
+        BEE_paths_values(index) = inf;
     end
 
     %Werbunek pszczó³ dla wybranych miejsc (proporcjonalnie do najlepszych miejsc + sta³a wartoœæ 1)
-    %workers = workers - paths_count;
-    val_per_worker = sum(best_paths_values-min(best_paths_values))/workers;
-    recruited_workers = zeros(1,paths_count);
+    %BEE_workers = BEE_workers - BEE_paths_count;
+    val_per_worker = sum(best_BEE_paths_values-min(best_BEE_paths_values))/BEE_workers;
+    recruited_BEE_workers = zeros(1,BEE_paths_count);
     if(val_per_worker==0)
-        for i=1:paths_count
-            recruited_workers(i) = 1+round(workers/paths_count);
+        for i=1:BEE_paths_count
+            recruited_BEE_workers(i) = 1+round(BEE_workers/BEE_paths_count);
         end
     else
-        for i=1:paths_count
-            recruited_workers(i) = 1+floor((best_paths_values(i)-min(best_paths_values))/val_per_worker);
+        for i=1:BEE_paths_count
+            recruited_BEE_workers(i) = 1+floor((best_BEE_paths_values(i)-min(best_BEE_paths_values))/val_per_worker);
         end
     end
-    %recruited_workers(1) = recruited_workers(1)+workers - sum(recruited_workers);
-    %workers = workers + paths_count;
+    %recruited_BEE_workers(1) = recruited_BEE_workers(1)+BEE_workers - sum(recruited_BEE_workers);
+    %BEE_workers = BEE_workers + BEE_paths_count;
 
     %Badanie przez pozosta³e pszczo³y s¹siedztwa ( ponowne wyliczanie funkcji celu )
-    potential_paths = best_paths;
-    paths_values = best_paths_values;
-    for i=1:paths_count
-        for j=1:recruited_workers(i)
-            potential_paths = [potential_paths get_neighbour(best_paths(:,i),n) ]; % czy droga moze sie powtarzac?
-            paths_values = [paths_values goal(potential_paths(:,end),n,gen_size,cost_matrix)];
+    BEE_potential_paths = best_paths;
+    BEE_paths_values = best_BEE_paths_values;
+    for i=1:BEE_paths_count
+        for j=1:recruited_BEE_workers(i)
+            BEE_potential_paths = [BEE_potential_paths get_neighbour(best_paths(:,i),BEE_n) ]; % czy droga moze sie powtarzac?
+            BEE_paths_values = [BEE_paths_values goal(BEE_potential_paths(:,end),BEE_n,BEE_gen_size,BEE_cost_matrix)];
         end
     end
 
     %Nowi zwiadowcy
-    current_length = length(paths_values);
-    potential_paths = [potential_paths zeros(gen_size+n-1,scouts)];
-    for i=1:scouts
-        potential_paths(:,current_length+i) = random_solution(gen_size,n)';
+    current_length = length(BEE_paths_values);
+    BEE_potential_paths = [BEE_potential_paths zeros(BEE_gen_size+BEE_n-1,BEE_scouts)];
+    for i=1:BEE_scouts
+        BEE_potential_paths(:,current_length+i) = random_solution(BEE_gen_size,BEE_n)';
     end
     best_scout=inf;
-    paths_values = [paths_values zeros(1,scouts)];
-    for i=1:scouts
-        tmp = goal(potential_paths(:,end-i+1),n,gen_size,cost_matrix);
+    BEE_paths_values = [BEE_paths_values zeros(1,BEE_scouts)];
+    for i=1:BEE_scouts
+        tmp = goal(BEE_potential_paths(:,end-i+1),BEE_n,BEE_gen_size,BEE_cost_matrix);
         if(best_scout>tmp) best_scout=tmp;end;
-        paths_values(current_length+i) = tmp;
+        BEE_paths_values(current_length+i) = tmp;
     end
 
     
     %Usuniecie powtorzonych tras
-    for i=1:length(paths_values)
-        tmp = paths_values(i);
+    for i=1:length(BEE_paths_values)
+        tmp = BEE_paths_values(i);
         for j=1:i
             if(i==j); continue; end;
-            if(paths_values(i)==paths_values(j))
-                paths_values(i)=inf;
+            if(BEE_paths_values(i)==BEE_paths_values(j))
+                BEE_paths_values(i)=inf;
             end
         end
     end
-    for i=1:length(paths_values)
-        if( paths_values(i)==inf)
-            paths_values = [paths_values(1:i-1) paths_values(i+1:end)];
-            potential_paths = [potential_paths(:,1:i-1) potential_paths(:,i+1:end)];
+    for i=1:length(BEE_paths_values)
+        if( BEE_paths_values(i)==inf)
+            BEE_paths_values = [BEE_paths_values(1:i-1) BEE_paths_values(i+1:end)];
+            BEE_potential_paths = [BEE_potential_paths(:,1:i-1) BEE_potential_paths(:,i+1:end)];
         end
-        if(i+1>length(paths_values)); break; end;
+        if(i+1>length(BEE_paths_values)); break; end;
     end
 
     %wybór i rysowanie najlepszej trasy
-	[v index] = min(best_paths_values);
-	if(v<best_solution_val)
-        best_solution = best_paths(:,index);
-        best_solution_val = v;
+	[v index] = min(best_BEE_paths_values);
+	if(v<BEE_best_solution_val)
+        BEE_best_solution = best_paths(:,index);
+        BEE_best_solution_val = v;
 	end;
 
 
     
-    out_best_solution = best_solution;
-    out_best_solution_val = best_solution_val; 
+    out_best_solution = BEE_best_solution;
+    out_best_solution_val = BEE_best_solution_val; 
     
     
     
