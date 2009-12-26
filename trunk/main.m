8615d964c9d9104a42bca90dcd8712c04a7600aa
feanaro666 <feanaro666@3277dffe-d080-11de-97ae-379497272b75>
2009-12-26 15:13:03 +0000

    Ostateczna z pszczelim

--- trunk/main.m
+++ trunk/main.m
@@ -1,23 +1,70 @@
 clc;
 clear all;
-n=2;
-[M points] = generate_matrix(10, n);
-global x y;
-x = points(:,1);
-y = points(:,2);
+close all;
+addpath alg-pszczeli;
 
+% ZADANIE PROBLEMU %
+global n size cost_matrix type;
+max_iter = 150;%maksymalna ilosc iteracji
+n = 3; %ilosc ciezarowek
+size = 20; %rozmiar miasta
+type = 1;% typ funkcji celu od 0 do 2 - odsylam do quality.m
 
-cd alg-pszczeli
+[cost_matrix points] = generate_matrix(size, n);
+%%%%%%%%%%%%%%%%%%%%
 
-a=[];
-b=[];
+% INICJALIZACJE ALGORYTMOW %
+%algorytm pszczeli
+BEE_routes=[];
+BEE_q=[];
+BEE_time=[];
+bee_init(cost_matrix,n,600,100,50);
 
-bee_init(M,n);
- 
-for k=1:40
-[tmpa tmpb] = bee_step; 
-a = [a tmpa];
-b = [b tmpb];
+%algorytm mrowkowy
+%TODO
+
+%algorytm genetyczny
+%TODO
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% GLOWNA PETLA ALGORYTMOW %
+for k=1:max_iter
+    %algorytm pszczeli    
+    tic;
+    [best_route best_q] = bee_step; 
+    BEE_time = [BEE_time toc];
+    BEE_routes = [BEE_routes best_route];
+    BEE_q = [BEE_q best_q];
+    
+    show_path( 1, best_route, points, n,0);
+    set(1,'Position',[0 -251 704 772]);
+    
+    figure(2);
+    hold on;
+    title('Best solution graph');
+    xlabel('Iteration');
+    ylabel('Quality function');
+    plot(BEE_q,'.b');
+    set(2,'Position',[725 167 704 354]);
+    grid on;
+    hold off;
+    
+    figure(3);
+    hold on;
+    title('Step time graph / Cumulative time');
+    xlabel('Iteration');
+    ylabel('Time [s]');
+    plot(BEE_time,'.b');
+    plot(k,sum(BEE_time),'.r');
+    set(3,'Position',[725 -238 704 320]);
+    grid on;
+    
+    %algorytm mrowkowy
+    %TODO
+
+    %algorytm genetyczny
+    %TODO
+
+    pause(0.01);
 end;
-cd ..
-show_path( 1, tmpa, points, n)
+
--- trunk/quality.m
+++ trunk/quality.m
@@ -1,52 +1,52 @@
-function Q = quality(X)
-% Funkcja oceny jakosci wektora rozwi¹zañ X
+function g=quality(r)
+%Funkcja celu
 %   r - wektor wejsciowy
-%   C - wektor pojemnosci ciezarowek
-%   c - wektor kosztow ciezarowek
-%   R - wektor maksymalnego czasu jazdy ciezarowek
+%   n - liczba ciezarowek
+%   size - rozmiar miasta
 %   cost_matrix - macierz kosztow
-%   orders - wektor wielkosci zamowien
-%   P = [przeladowanie, nadgodziny] - wspolczynniki kary - opcjonalnie 
+%   type - typ funkcji celu
 
-global C c R cost_matrix orders P
+global n size cost_matrix type;
 
-len = length(X);	% Dlugosc chromosomu
-Cn = length(C);		% Liczba ciezarowek
+%1. Najprostrza funkcja celu - suma kosztów
+if(type==0)
+    g = 0;
+    r=r';
+    for i=1:size+n-2
+       g = g + cost_matrix(r(i),r(i+1));        
+    end
+    g = g+cost_matrix(r(end),1);
+end;
 
-first = 0;
-car = 0;
-for i = 1:len,	% Find first car
-	if X(i) <= Cn,
-		car = X(i);	% Select car
-		first = i;	% Select first car position
-		break
-	end
-end
-
-X = [X(first:end), X(1:first)];	% Rotate chromosome X and close cycle
-
-Q = 0;
-L = 0;	% Car load
-r = 0;	% Length of route
-
-for i = 1:len+1,
-	%Q = Q + c(car)*cost_matrix(X(i)X(i+1));
-	if X(i) <= Cn,	% Car is back in magazine
-		Q = Q + c(car)*r;	% Add cost of transport using actual car
-		if L > C(car),	% Add punishment for exceeding car load capacity
-			Q = Q + P(1)*(L - C(car));
-		end
-		if r > R(car),	% Add punishment for exceeding maximum length of car route (worktime)
-			Q = Q + P(2)*(r - R(car));
-		end
-		car = X(i);	% Select new car
-		L = 0;
-		r = 0;
-	else
-		L = L + orders(X(i));	% Actualize load
-		r = r + cost_matrix(X(i), X(i+1));	% Actualize length of route
-	end
-% 	disp ([X(i) car Q L r])
-end
-
-end
+%2. Narastaj¹ca funkcja celu dla coraz d³u¿szych tras - kara za zbyt d³ugi
+%czas
+if(type==1)
+    g = 0;
+    r=r';
+    
+    for i=1:size+n-2
+        if (r(i)<=n); punish = 0; end;
+        punish = punish +1;
+        g = g + punish*cost_matrix(r(i),r(i+1));        
+    end
+    g = g+cost_matrix(r(end),1);    
+end;
+    
+%3. Ograniczenie ³adownoœci do sta³ej wartoœci - po przekroczeniu
+%³adownoœci nie ma mo¿liwoœci innej ni¿ powrót
+if(type==2)
+    g = 0;
+    r=r';
+    moves_count = 0;
+    max_moves = 20;
+    for i=1:size+n-2
+        if (r(i)<=n); moves_count = 0; end;
+        moves_count = moves_count +1;
+        if( moves_count >= max_moves)
+            g = g + cost_matrix(r(i),r(i+1)) + inf;    
+        else
+            g = g + cost_matrix(r(i),r(i+1));    
+        end;
+    end
+    g = g+cost_matrix(r(end),1);  
+end;
